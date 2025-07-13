c24777b88cf9f922e6c404caee59f5afcdf8ca59
Tejas Hirve <tejashirve11@gmail.com>
2025-07-14 01:47:33 +0530

    Add Prisma schema and setup

---
+++ src/read.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function getUser(username: string) {
+  const user = await prisma.user.findFirst({
+    where: {
+        username: username
+    }
+  })
+  console.log(user);
+}
+
+getUser("admin1");
